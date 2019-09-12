878e5a2ea2342026638b739fdcff4d320f9ac4de
Street Hawk <rayyan.saeed@outlook.com>
2019-09-12 10:10:16 -0700

    change pass

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
         'USER': 'postgres',
-        'PASSWORD': 'California_92',
+        'PASSWORD': 'django1234',
         'HOST': 'localhost',
         'PORT': '5432',
     }
