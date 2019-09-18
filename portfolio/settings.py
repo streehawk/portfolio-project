47a9bf5270de8e2cac8bc6ba1930da2b442b3a15
Rayyan Saeed <rayyansaeed.work@gmail.com>
2019-09-18 02:25:59 +0000

    finally works on domain name

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
@@ -79,8 +78,8 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'portfoliodb',
-        'USER': 'postgres',
-        'PASSWORD': 'django1234',
+        'USER': 'djangodbman',
+        'PASSWORD': 'california_92',
         'HOST': 'localhost',
         'PORT': '5432',
     }
